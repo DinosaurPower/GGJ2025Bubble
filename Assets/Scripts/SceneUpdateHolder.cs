76e79f581c2bf2972b52f266eb73eb47d1809e35
DinosaurPower <lysym685@newschool.edu>
2025-01-26 02:00:58 -0500

    made scene

--- Assets/Scripts/SceneUpdateHolder.cs
+++ Assets/Scripts/SceneUpdateHolder.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class SceneUpdateHolder : MonoBehaviour
 {
     public GameObject[] scenes;
+    public GameObject[] canvasProps;
     public int sceneN;
     public void updateScenes (int Order){
         scenes[sceneN].gameObject.SetActive(false); 
+        canvasProps[sceneN].gameObject.SetActive(false);
         sceneN+= Order;
-        if (sceneN >3){sceneN = 0;}
-        if (sceneN <0){sceneN = 3;}
+        if (sceneN >2){sceneN = 0;}
+        if (sceneN <0){sceneN = 2;}
         scenes[sceneN].gameObject.SetActive(true);
+        canvasProps[sceneN].gameObject.SetActive(true);
+        
     }
 }
