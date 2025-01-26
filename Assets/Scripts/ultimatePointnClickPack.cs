eb026f37a803d509d344322d30108427b136a1c6
DinosaurPower <lysym685@newschool.edu>
2025-01-26 15:40:26 -0500

    added outro (idk didnt test)

--- Assets/Scripts/ultimatePointnClickPack.cs
+++ Assets/Scripts/ultimatePointnClickPack.cs
@@ -10,8 +10,11 @@ public class ultimatePointnClickPack : MonoBehaviour
     public DialogueManager dialogueManager;
     public Dialogue dialogue;
     public Dialogue inactiveDialogue;
+    public Dialogue otherDialogue;
     public GameObject nametag;
     public ParameterHolder parameterHolder;
+
+    public GameObject everythingToTurnOff;
 
     public void OnPointerEnter(PointerEventData eventData){
         nametag.SetActive(true);
@@ -32,15 +35,27 @@ public class ultimatePointnClickPack : MonoBehaviour
     public void getOut(){
 
        Debug.Log("Here");
-        if (parameterHolder.Key1 && parameterHolder.Key2 && parameterHolder.Key3){
+        if (parameterHolder.Key1 && parameterHolder.Key2){
           revealItem(true);
-            dialogueManager.StartDialogue(dialogue);
+            dialogueManager.StartDialogue(dialogue, ()=> {LightsOut();});
             Debug.Log("Bark");
 
         } else {dialogueManager.StartDialogue(inactiveDialogue);
         Debug.Log("Meow");
         }
     }
+ 
+ public void LightsOut(){
 
+dialogueManager.StartDialogue(otherDialogue);
+
+everythingToTurnOff.SetActive(true);
 
 }
+
+
+ }
+
+
+
+
