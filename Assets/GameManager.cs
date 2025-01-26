ca6db52cdbad4e8feefcd40fbdcc0a1337fd738c
DinosaurPower <lysym685@newschool.edu>
2025-01-26 10:53:52 -0500

    made game manager retain between scenes

---
+++ Assets/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Add any additional methods or properties needed for your game manager.
+}
+
